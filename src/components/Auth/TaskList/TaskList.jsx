2ef03b18c905b007a25272ceecddd6b7b4caa449
Nikesh <info.nikesh12@gmail.com>
2025-06-16 14:20:50 +0545

    ems management

---
+++ src/components/Auth/TaskList/TaskList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TaskList = () => {
+  return (
+    <div id='tasklist' className="h-[55%] overflow-x-auto w-full mt-10 rounded-lg py-5 flex items-center justify-start gap-5 flex-nowrap">
+      <div className="h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0"></div>
+      <div className="h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0"></div>
+      <div className="h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0"></div>
+      <div className="h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0"></div>
+      <div className="h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0"></div>
+      <div className="h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0"></div>
+      <div className="h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0"></div>
+    </div>
+  );
+};
+
+export default TaskList;
