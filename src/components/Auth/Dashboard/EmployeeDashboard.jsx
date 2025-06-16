f10720b5c4b35afa29fe50134d2b5d53a3161aa8
Nikesh <info.nikesh12@gmail.com>
2025-06-16 13:18:02 +0545

    ems management

--- src/components/Auth/Dashboard/EmployeeDashboard.jsx
+++ src/components/Auth/Dashboard/EmployeeDashboard.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Header from '../Header'
+import TaskListNumber from '../TaskListNumber'
+import TaskList from '../TaskList/TaskList'
 
 const EmployeeDashboard = () => {
   return (
-    <div>EmployeeDashboard</div>
+    <div className='p-10 bg-[black] h-screen'>
+      <Header/>
+      <TaskListNumber/>
+      <TaskList/>
+    </div>
   )
 }
 
