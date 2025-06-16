887d979827d2196f5e1024d31fccb3901fab3408
Nikesh <info.nikesh12@gmail.com>
2025-06-16 13:18:50 +0545

    ems management

---
+++ src/components/Auth/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex items-end justify-between text-white text-2xl font-medium'>
+       <h1>Hello <br/> <span className='text-3xl font-semibold'>Nikesh</span> </h1> 
+       <button className='bg-red-600 px-5 py-2 rounded-sm text-lg font-medium' >Logout</button>
+    </div>
+  )
+}
+
+export default Header
