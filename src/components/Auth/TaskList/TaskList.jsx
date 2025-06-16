d0e44f1158930210dbdef4c727c009b5d263acac
Nikesh <info.nikesh12@gmail.com>
2025-06-16 14:52:27 +0545

    ems management

--- src/components/Auth/TaskList/TaskList.jsx
+++ src/components/Auth/TaskList/TaskList.jsx
@@ -2,14 +2,122 @@ import React from "react";
 
 const TaskList = () => {
   return (
-    <div id='tasklist' className="h-[55%] overflow-x-auto w-full mt-10 rounded-lg py-5 flex items-center justify-start gap-5 flex-nowrap">
-      <div className="h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0"></div>
-      <div className="h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0"></div>
-      <div className="h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0"></div>
-      <div className="h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0"></div>
-      <div className="h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0"></div>
-      <div className="h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0"></div>
-      <div className="h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0"></div>
+    <div
+      id="tasklist"
+      className="h-[55%] overflow-x-auto w-full  mt-10 rounded-lg py-5 flex items-center justify-start gap-5 flex-nowrap"
+    >
+      <div className="h-full w-[300px]  bg-red-400 rounded-xl flex-shrink-0 p-5  overflow-hidden">
+        <div className=" flex justify-between items-center">
+          <h3 className="bg-red-500  px-3 py-2 rounded-md text-sm text-white">
+            High
+          </h3>
+          <h4 className="text-white">July 2025</h4>
+        </div>
+        <h2 className="mt-5 text-md font-semibold text-white">
+          Make Simple User Interface EMS{" "}
+        </h2>
+        <p className="text-sm text-white mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+          exercitationem aliquam quam nam laudantium recusandae tempora? Sed,
+          
+        </p>
+      </div>
+      <div className="h-full w-[300px] bg-yellow-400 rounded-xl flex-shrink-0 p-5">
+         <div className=" flex justify-between items-center">
+          <h3 className="bg-red-500  px-3 py-2 rounded-md text-sm text-white">
+            High
+          </h3>
+          <h4 className="text-white">July 2025</h4>
+        </div>
+        <h2 className="mt-5 text-md font-semibold text-white">
+          Make Simple User Interface EMS{" "}
+        </h2>
+        <p className="text-sm text-white mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+          exercitationem aliquam quam nam laudantium recusandae tempora? Sed,
+          
+        </p>
+      </div>
+      <div className="h-full w-[300px] bg-blue-400 rounded-xl flex-shrink-0 p-5">
+        <div className=" flex justify-between items-center">
+          <h3 className="bg-red-500  px-3 py-2 rounded-md text-sm text-white">
+            High
+          </h3>
+          <h4 className="text-white">July 2025</h4>
+        </div>
+        <h2 className="mt-5 text-md font-semibold text-white">
+          Make Simple User Interface EMS{" "}
+        </h2>
+        <p className="text-sm text-white mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+          exercitationem aliquam quam nam laudantium recusandae tempora? Sed,
+          
+        </p>
+      </div>
+      <div className="h-full w-[300px] bg-green-400 rounded-xl flex-shrink-0 p-5">
+        <div className=" flex justify-between items-center">
+          <h3 className="bg-red-500  px-3 py-2 rounded-md text-sm text-white">
+            High
+          </h3>
+          <h4 className="text-white">July 2025</h4>
+        </div>
+        <h2 className="mt-5 text-md font-semibold text-white">
+          Make Simple User Interface EMS{" "}
+        </h2>
+        <p className="text-sm text-white mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+          exercitationem aliquam quam nam laudantium recusandae tempora? Sed,
+          
+        </p>
+      </div>
+      <div className="h-full w-[300px] bg-sky-400 rounded-xl flex-shrink-0 p-5">
+        <div className=" flex justify-between items-center">
+          <h3 className="bg-red-500  px-3 py-2 rounded-md text-sm text-white">
+            High
+          </h3>
+          <h4 className="text-white">July 2025</h4>
+        </div>
+        <h2 className="mt-5 text-md font-semibold text-white">
+          Make Simple User Interface EMS{" "}
+        </h2>
+        <p className="text-sm text-white mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+          exercitationem aliquam quam nam laudantium recusandae tempora? Sed,
+          
+        </p>
+      </div>
+      <div className="h-full w-[300px] bg-orange-400 rounded-xl flex-shrink-0 p-5">
+        <div className=" flex justify-between items-center">
+          <h3 className="bg-red-500  px-3 py-2 rounded-md text-sm text-white">
+            Low
+          </h3>
+          <h4 className="text-white">July 2025</h4>
+        </div>
+        <h2 className="mt-5 text-md font-semibold text-white">
+          Make Simple User Interface EMS{" "}
+        </h2>
+        <p className="text-sm text-white mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+          exercitationem aliquam quam nam laudantium recusandae tempora? Sed,
+          
+        </p>
+      </div>
+      <div className="h-full w-[300px] bg-black-400 rounded-xl flex-shrink-0">
+        <div className=" flex justify-between items-center">
+          <h3 className="bg-red-500  px-3 py-2 rounded-md text-sm text-white">
+            High
+          </h3>
+          <h4 className="text-white">July 2025</h4>
+        </div>
+        <h2 className="mt-5 text-md font-semibold text-white">
+          Make Simple User Interface EMS{" "}
+        </h2>
+        <p className="text-sm text-white mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+          exercitationem aliquam quam nam laudantium recusandae tempora? Sed,
+          
+        </p>
+      </div>
     </div>
   );
 };
