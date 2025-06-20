920f42f82afd8090b1fa5e54add52ec864b87f90
Nikesh <info.nikesh12@gmail.com>
2025-06-21 00:03:31 +0545

    ems

--- src/components/Auth/Header.jsx
+++ src/components/Auth/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='flex items-end justify-between text-white text-2xl font-medium'>
+    <div className='flex items-end justify-between text-white text-2xl font-medium px-5 bg-black'>
        <h1>Hello <br/> <span className='text-3xl font-semibold'>Nikesh</span> </h1> 
        <button className='bg-red-600 px-5 py-2 rounded-sm text-lg font-medium' >Logout</button>
     </div>
