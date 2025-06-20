e7a55a043d1f19586b40170b8f4d305598161774
Nikesh <info.nikesh12@gmail.com>
2025-06-21 00:08:43 +0545

    ems

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Login from "./components/Auth/Login";
+import EmployeeDashboard from "./components/Auth/Dashboard/EmployeeDashboard";
+import AdminDashboard from "./components/Auth/Dashboard/AdminDashboard";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Alltask from "./components/Auth/Alltask";
+import {setLocalStorage} from "./utils/localStorage"
+import {getLocalStorage} from "./utils/localStorage"
+
+
+
 const App = () => {
+  useEffect(()=>{
+    // setLocalStorage();
+    getLocalStorage();
+
+  },[])
   return (
     <>
     <Login/>
+    setLocalStorage
     </>
   );
 };
