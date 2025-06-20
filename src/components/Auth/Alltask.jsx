8a7dfcb9def06125dbccc1fb5a5f6316dda858d6
Nikesh <info.nikesh12@gmail.com>
2025-06-20 22:29:03 +0545

    ems

---
+++ src/components/Auth/Alltask.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Alltask = () => {
+  return (
+    <div className="bg-[#1c1c1c] p-5 text-white mt-5 rounded h-48 overflow-auto">
+      <div className="bg-red-400 mb-3 py-2 px-4 flex justify-between rounded">
+        <h3>Nikesh</h3>
+        <h3>Make Ui Design</h3>
+        <h3>Status</h3>
+      </div>
+      <div className="bg-yellow-400 mb-3 py-2 px-4 flex justify-between rounded">
+        <h3>Nikesh</h3>
+        <h3>Make Ui Design</h3>
+        <h3>Status</h3>
+      </div>
+      <div className="bg-sky-400 mb-3 py-2 px-4 flex justify-between rounded">
+        <h3>Nikesh</h3>
+        <h3>Make Ui Design</h3>
+        <h3>Status</h3>
+      </div>
+        <div className="bg-orange-400 mb-3 py-2 px-4 flex justify-between rounded">
+        <h3>Nikesh</h3>
+        <h3>Make Ui Design</h3>
+        <h3>Status</h3>
+      </div>
+        <div className="bg-sky-800 mb-3 py-2 px-4 flex justify-between rounded">
+        <h3>Nikesh</h3>
+        <h3>Make Ui Design</h3>
+        <h3>Status</h3>
+      </div>
+        <div className="bg-green-400 mb-3 py-2 px-4 flex justify-between rounded">
+        <h3>Nikesh</h3>
+        <h3>Make Ui Design</h3>
+        <h3>Status</h3>
+      </div>
+        <div className="bg-green-400 mb-3 py-2 px-4 flex justify-between rounded">
+        <h3>Nikesh</h3>
+        <h3>Make Ui Design</h3>
+        <h3>Status</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Alltask;
