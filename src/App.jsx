54772543b86ae7cc4c44d84fccd4f810af748745
Nikesh <info.nikesh12@gmail.com>
2025-06-21 14:17:53 +0545

    ems

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Login from "./components/Auth/Login";
 import EmployeeDashboard from "./components/Auth/Dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/Auth/Dashboard/AdminDashboard";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Alltask from "./components/Auth/Alltask";
-import {setLocalStorage} from "./utils/localStorage"
-import {getLocalStorage} from "./utils/localStorage"
-
-
+// import {setLocalStorage} from "./utils/localStorage"
+// import {getLocalStorage} from "./utils/localStorage"
 
 const App = () => {
-  useEffect(()=>{
-    // setLocalStorage();
-    getLocalStorage();
+  const [user, setUser] = useState(null);
 
-  },[])
+  const loginHandle = (email, password) => {
+    if (email == "admin@gmail.com" && password == "123") {
+      setUser("admin");
+    } else if (email == "user@gmail.com" && password == "123") {
+      setUser("employees");
+    } else {
+      alert("Invalid");
+    }
+  };
+
   return (
     <>
-    <Login/>
-    setLocalStorage
+      {!user ? <Login loginHandle={loginHandle} /> : ""}
+      {user == "admin" ? <AdminDashboard /> : <EmployeeDashboard />}
     </>
   );
 };
-
 export default App;
