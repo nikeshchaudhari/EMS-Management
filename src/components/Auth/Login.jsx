9bf059e711d8d699c5249e3039cbbd03a6117bfe
Nikesh <info.nikesh12@gmail.com>
2025-06-21 14:18:09 +0545

    ems

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { useState } from "react";
 
-const Login = () => {
+const Login = ({loginHandle}) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log("email is",email);
-    console.log("password is ",password);
-    setEmail("");
-    setPassword("");
+    // console.log("email is",email);
+    // console.log("password is ",password);
+    // setEmail("");
+    // setPassword("");
+    loginHandle(email,password)
     
   };
   return (
